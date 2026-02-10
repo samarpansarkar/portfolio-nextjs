4d7a75d478c488d56ce1fd9c24c7b4a6b4570256
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2026-02-10 13:42:22 +0530

    add: SkillSlice. add: in store add skillReducer.

---
+++ src/redux/slices/skillSlice.js
@@ -0,0 +1,55 @@
+const { default: API } = require("@/utils/AxiosInstance");
+const { createSlice } = require("@reduxjs/toolkit");
+const { createAsyncThunk } = require("@reduxjs/toolkit");
+
+const fetchSkills = createAsyncThunk(
+  "skills/fetch",
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await API.get("/skills");
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(
+        error?.response?.data?.message || "Faild to fetch skills.",
+      );
+    }
+  },
+  //!Request caching condition (avoid double fetch)
+  {
+    condition: (_, { getState }) => {
+      const { skills } = getState();
+
+      if (skills.loading) return false;
+      if (skills.projects.length) return false;
+    },
+  },
+);
+
+const initialState = {
+  skills: [],
+  loading: false,
+  error: null,
+};
+
+const skillSlice = createSlice({
+  name: "skills",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSkills.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchSkills.fulfilled, (state, action) => {
+        state.loading = false;
+        state.projects = action.payload;
+      })
+      .addCase(fetchSkills.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default skillSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import porjectReducer from "@/redux/slices/ProjectSlice";
+import skillsReducer from "@/redux/slices/skillSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = () => {
   return configureStore({
     reducer: {
       projects: porjectReducer,
+      skills: skillsReducer,
     },
     devTools: process.env.NODE_ENV !== "production",
   });
