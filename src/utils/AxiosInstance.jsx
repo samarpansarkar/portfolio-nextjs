665c624392dd0374ad3c1cb71815f183665be789
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2026-02-10 12:46:35 +0530

    Setup: the project repo. Add : API instance(AXIOS)

---
+++ src/utils/AxiosInstance.jsx
@@ -0,0 +1,33 @@
+const { default: axios } = require("axios");
+
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+AxiosInstance.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    console.error("API Error:", error.response?.data || error.message);
+    return Promise.reject(error);
+  },
+);
+
+export default API;
